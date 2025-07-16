a8873c5ae96d3bcab9eaa099b41c0dc15805001e
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2025-07-16 09:55:59 +0200

    samples: matter: Allow to generate ZAP files at build time
    Added a new choice and kconfig options to allow generate ZAP files
    at build time.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- samples/matter/common/cmake/data_model.cmake
+++ samples/matter/common/cmake/data_model.cmake
@@ -17,10 +17,29 @@ function(ncs_configure_data_model)
     ${zap_parent_dir}
   )
 
-  chip_configure_data_model(matter-data-model
-    BYPASS_IDL
-    GEN_DIR ${zap_parent_dir}/zap-generated
-    ZAP_FILE ${zap_file_path}
-    EXTERNAL_CLUSTERS ${ARG_EXTERNAL_CLUSTERS}
-  )
+  if(CONFIG_NCS_SAMPLE_MATTER_DOWNLOAD_ZAP_CLI)
+    # We require zap tool for this to work, so request downloading it if not present.
+    find_program(ZAP_CLI_EXECUTABLE zap-cli)
+    if(NOT ZAP_CLI_EXECUTABLE)
+      execute_process(COMMAND -c "west zap-install")
+      message(STATUS "zap-cli installed in: ${ZAP_CLI_EXECUTABLE}")
+    else ()
+      message(STATUS "zap-cli found at: ${ZAP_CLI_EXECUTABLE}")
+    endif()
+  endif()
+
+  if(CONFIG_MATTER_ZAP_GENERATION_STATIC)
+    chip_configure_data_model(matter-data-model
+      BYPASS_IDL
+      GEN_DIR ${zap_parent_dir}/zap-generated
+      ZAP_FILE ${zap_file_path}
+      EXTERNAL_CLUSTERS ${ARG_EXTERNAL_CLUSTERS}
+    )
+  elseif(CONFIG_MATTER_ZAP_GENERATION_BUILD_TIME)
+    chip_configure_data_model(app
+      ZAP_FILE ${zap_file_path}
+      EXTERNAL_CLUSTERS ${ARG_EXTERNAL_CLUSTERS}
+    )
+  endif()
+
 endfunction()
